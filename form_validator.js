5c861755570abcf3d8f6d83797e02b23854b0d23
yriy yriy <yriybog2003@gmail.com>
2023-05-31 20:34:15 +0300

    validation

--- form_validator.js
+++ form_validator.js
@@ -10,6 +10,13 @@ form.addEventListener("change", eventHandler);
 form.addEventListener("invalid", eventHandler, true);
 
 
+const fname = document.querySelector('#fname');
+const lname = document.querySelector('#lname');
+const email = document.querySelector('#email');
+const textarea = document.querySelector('#textarea');
+
+
+
 function eventHandler(e) {
     if (e.target.tagName != "INPUT" && e.target.tagName != "TEXTAREA") {
         return;
@@ -18,14 +25,120 @@ function eventHandler(e) {
     checkErrorAndSetMessage(e.target);
 }
 
+const forbidden = ['#', '_', '@', '$', '%', '^', '&', '*', '.', '/', "'", '"', '`'];
+
+function checkNumbers(inputText) {
+    for (var i = 0; i < inputText.length; i++) {
+      if (!isNaN(parseInt(inputText[i]))) {
+        return true;
+      }
+    }
+    return false;
+}
+
+function checkLenght(inputText, min){
+    if(inputText.length > min){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+function checkForbidden(inputText){
+    for (let i = 0; i < inputText.length; i++){
+        for(let j =0; j < forbidden.length; j++){
+            if(inputText[i] === forbidden[j]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+function checkMail(inputText){
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailPattern.test(inputText)) {
+      return false;
+    } else {
+        return true;
+    }
+}
+
+
 function checkErrorAndSetMessage(input) {
     if (input.validity.valueMissing) {
         input.setCustomValidity("This field is required!");
         input.classList.add("invalid");
-    } else {
+    } 
+    else if(input.id === 'lname'){
+        if(checkNumbers(input.value)){
+            input.setCustomValidity("This input can`t contain digits!");
+            input.classList.add("invalid");
+        }
+        else if(checkLenght(input.value, 5)){
+            input.setCustomValidity("Input lenght must be > 5 symbols");
+            input.classList.add("invalid");
+        }
+        else if(checkForbidden(input.value)){
+            input.setCustomValidity("Input contains forbidden value!");
+            input.classList.add("invalid");
+        }
+        else {
+            input.setCustomValidity("");
+            input.classList.remove("invalid");
+        }
+       
+        
+    }
+    else if(input.id === 'fname'){
+        if(checkNumbers(input.value)){
+            input.setCustomValidity("This input can`t contain digits!");
+            input.classList.add("invalid");
+        }
+        else if(checkLenght(input.value, 5)){
+            input.setCustomValidity("Input lenght must be > 5 symbols");
+            input.classList.add("invalid");
+        }
+        else if(checkForbidden(input.value)){
+            input.setCustomValidity("Input contains forbidden value!");
+            input.classList.add("invalid");
+        }
+        else {
+            input.setCustomValidity("");
+            input.classList.remove("invalid");
+        }
+       
+       
+        
+    }
+    else if(input.id === 'textarea'){
+        if(checkLenght(input.value, 50)){
+            input.setCustomValidity("Input lenght must be > 5 symbols");
+            input.classList.add("invalid");
+        }
+        else {
+            input.setCustomValidity("");
+            input.classList.remove("invalid");
+        }
+    }
+    else if(input.id === 'email'){
+        if(checkLenght(input.value, 8)){
+            input.setCustomValidity("Input lenght must be > 5 symbols");
+            input.classList.add("invalid");
+        }
+        else if(checkMail(input.value)){
+            input.setCustomValidity("Incorrect email");
+            input.classList.add("invalid");
+        }
+        else {
+            input.setCustomValidity("");
+            input.classList.remove("invalid");
+        }
+    }  
+    else {
         input.setCustomValidity("");
         input.classList.remove("invalid");
     }
+    
 }
 
 
